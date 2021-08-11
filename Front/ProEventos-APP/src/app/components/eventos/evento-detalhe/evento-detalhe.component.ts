3412b950f9b46d18f15a5cac763c029484319b8c
Diego Barbosa <diego1279.dr@gmail.com>
2021-08-11 16:40:58 -0300

    Seção 8: Reactive Forms e Mudança Layout

--- Front/ProEventos-APP/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-APP/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { timeStamp } from 'node:console';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
-  styleUrls: ['./evento-detalhe.component.scss']
+  styleUrls: ['./evento-detalhe.component.scss'],
 })
 export class EventoDetalheComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public validation(): void {
+    this.form = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.maxLength(12000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: ['', Validators.required],
+    });
+  }
+
+  public resetForm(): void {
+    this.form.reset();
   }
 
 }
