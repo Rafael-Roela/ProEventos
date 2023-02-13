560ab451296585ac280e3b944356c00b7f9cefdd
Diego Barbosa <diego1279.dr@gmail.com>
2023-02-13 15:47:18 -0300

    Seção 10 - Angular - Registrando Eventos

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -111,9 +111,9 @@ namespace ProEventos.API.Controllers
                 var evento = await _eventoService.GetEventoByIdAsync(id, true);
                 if (evento == null) return NoContent();
 
-                return await _eventoService.DeleteEvento(id) ? 
-                       Ok("Deletado") : 
-                       throw new Exception("Ocorreu um erro não essspecífico ao tentar deletar o evento.");
+                return await _eventoService.DeleteEvento(id)
+                      ? Ok(new { messagem = "Deletado" })
+                      : throw new Exception("Ocorreu um erro não essspecífico ao tentar deletar o evento.");
             }
             catch (Exception ex)
             {
